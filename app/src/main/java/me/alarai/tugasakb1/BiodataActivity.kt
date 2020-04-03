a24a76e324d965a29b2ac3a15a3d90d9a5d80858
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-04-04 04:53:05 +0700

    Biodata activity & say hi activity

---
+++ app/src/main/java/me/alarai/tugasakb1/BiodataActivity.kt
@@ -0,0 +1,18 @@
+package me.alarai.tugasakb1
+/*
+* NIM : 10116006
+* Nama: Ala Rai AbdiAllah
+* Kelas: IF-6K
+* Tanggal Pengerjaan : 4 April 2020
+* Deskripsi Pekerjaan: Halaman input biodata
+* */
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BiodataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+    }
+}
---
+++ app/src/main/java/me/alarai/tugasakb1/GreetActivity.kt
@@ -0,0 +1,18 @@
+package me.alarai.tugasakb1
+/*
+* NIM : 10116006
+* Nama: Ala Rai AbdiAllah
+* Kelas: IF-6K
+* Tanggal Pengerjaan : 4 April 2020
+* Deskripsi Pekerjaan: Halaman Say Hi!
+* */
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GreetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greet)
+    }
+}
