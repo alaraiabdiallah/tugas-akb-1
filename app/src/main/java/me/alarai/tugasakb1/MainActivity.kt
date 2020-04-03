4982976b56bf5d76165caae44390bbbd775f810d
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-04-04 03:23:20 +0700

    first commit

---
+++ app/src/main/java/me/alarai/tugasakb1/MainActivity.kt
@@ -0,0 +1,23 @@
+package me.alarai.tugasakb1
+/*
+* NIM : 10116006
+* Nama: Ala Rai AbdiAllah
+* Kelas: IF-6K
+* Tanggal Pengerjaan : 2 April 2020
+* Deskripsi Pekerjaan: Halaman Pertama
+* */
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
