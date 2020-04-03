30a9d91e2e7b822137aed06d6f0b257a80721424
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-04-04 04:38:18 +0700

    login activity

---
+++ app/src/main/java/me/alarai/tugasakb1/LoginCodeActivity.kt
@@ -0,0 +1,18 @@
+package me.alarai.tugasakb1
+/*
+* NIM : 10116006
+* Nama: Ala Rai AbdiAllah
+* Kelas: IF-6K
+* Tanggal Pengerjaan : 4 April 2020
+* Deskripsi Pekerjaan: Halaman login
+* */
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_code)
+    }
+}
--- app/src/main/java/me/alarai/tugasakb1/MainActivity.kt
+++ app/src/main/java/me/alarai/tugasakb1/MainActivity.kt
@@ -5,12 +5,13 @@ package me.alarai.tugasakb1
 * Kelas: IF-6K
 * Tanggal Pengerjaan : 2 April 2020
 * Deskripsi Pekerjaan: Halaman Pertama
+* Tanggal Pengerjaan : 4 April 2020
+* Deskripsi Pekerjaan: Custom Button
 * */
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mulai_button.setOnClickListener {
+            val intent = Intent(this, LoginCodeActivity::class.java).apply {}
+            startActivity(intent)
+        }
     }
+
 }
